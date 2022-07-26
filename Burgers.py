61047c8bb410adf001fa1acc8a9b301d91527c3f
InTruder.Sec <75447837+InTruder-Sec@users.noreply.github.com>
2022-07-26 12:36:50 +0530

    Create Burgers.py

---
+++ Burgers.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t = int(input())
+arr = []
+for i in range(t):
+    m = input()
+    m = m.split()
+    arr.append(m)
+for i in range(t):
+    if int(arr[i][0])<=int(arr[i][1]):
+        print(arr[i][0])
+    else:
+        print(arr[i][1])

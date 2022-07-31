b4e757d358d4bf85f42999fe095fbdd8663c7eef
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-07-31 18:31:45 +0530

    Create cabs.py

---
+++ cabs.py
@@ -0,0 +1,14 @@
+# cook your dish here
+t = int(input())
+arr = []
+for i in range(t):
+    m = input()
+    m = m.split()
+    arr.append(m)
+for i in range(t):
+    if int(arr[i][0])>int(arr[i][1]):
+        print("SECOND")
+    elif int(arr[i][0]) == int(arr[i][1]):
+        print("ANY")
+    else:
+        print("FIRST")

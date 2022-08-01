34cbbd4f34e112f345651726e726f02c8364e77f
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-08-01 15:53:18 +0530

    Create expiry.py

---
+++ expiry.py
@@ -0,0 +1,13 @@
+# cook your dish here
+t = int(input())
+arr = []
+for i in range(t):
+    m = input()
+    m = m.split()
+    arr.append(m)
+for i in range(t):
+    last = int(arr[i][2])*int(arr[i][1])
+    if int(arr[i][0])>last:
+        print("NO")
+    else:
+        print("YES")

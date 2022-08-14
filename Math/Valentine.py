2c045fab8460ba02a110e0e302aeca1de79b904d
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-08-14 23:33:01 +0530

    Create Valentine.py

---
+++ Math/Valentine.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t = int(input())
+arr = []
+for i in range(t):
+    m = input()
+    m = m.split()
+    arr.append(m)
+for i in range(t):
+    total = int(arr[i][0])//int(arr[i][1])
+    print(total)
